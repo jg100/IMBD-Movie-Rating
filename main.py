b75daa7bc22099b7cbe9a5dbf97dc23ff3c7c22f
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2022-03-09 13:58:52 -0800

    remove special character leaving only words

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
+import re
 
 directoryPos = "./aclImdb/train/pos"
 
@@ -12,27 +14,48 @@ def remove_stop_words(tokenized_text):
     stop_words = set(stopwords.words("english"))
     tokenized_set = set(tokenized_text)
     set_difference = tokenized_set - stop_words
-    print(set_difference)
+    # print(set_difference)
     return tokenized_set - stop_words
 
 
 # Traverse pos train folder text files
-for filename in os.listdir(directoryPos):
-    if filename.endswith(".txt"):
+posReviewList = os.listdir(directoryPos)
+for x in range(10):
+    if posReviewList[x].endswith(".txt"):
+        
         # Open the text file
-        filePath = directoryPos + "/" + filename
-        file = open(filePath, "r")
+        filePath = directoryPos + "/" + posReviewList[x]
+        file = open(filePath, "r", encoding="utf8")
 
         # tokenize data
-        text_data = file.read().lower().split(' ')
+        text_review = file.read()
+        text_review = text_review.lower()
+
+        # remove special characters leaving only word and apostrophe
+        text_review = re.sub("[^A-Za-z']+",' ',text_review)
+        text_data = text_review.split(' ')
 
         #text_data = remove_stop_words(text_data)
         tokenized_pos += text_data
 
-        continue
-    else:
-        continue
+print(len(tokenized_pos))
+tokenized_pos = list(remove_stop_words(tokenized_pos))
+
+
+# for filename in os.listdir(directoryPos):
+#     if filename.endswith(".txt"):
+#         # Open the text file
+#         filePath = directoryPos + "/" + filename
+#         file = open(filePath, "r", encoding="utf8")
+
+#         # tokenize data
+#         text_data = file.read().lower().split(' ')
+
+#         #text_data = remove_stop_words(text_data)
+#         tokenized_pos += text_data
+
+#         continue
+#     else:
+#         continue
 
 print(len(tokenized_pos))
-
-
